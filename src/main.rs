63cca9b9669b181a00490bd115a8b4bbaae8922b
Harry-Hopkinson <harryjhopkinson@gmail.com>
2023-06-13 15:42:06 +0000

    Format Code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
 use serde_json;
+use std::fs::File;
 use std::io;
 
 mod player;
@@ -18,12 +18,32 @@ fn main() {
 
     let mut input = String::new();
     println!("Strict mode? (y/n)");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
     strict = input.trim() == "y";
 
     for player in &players {
         if player.POS == "CB" || player.POS == "LB" || player.POS == "RB" {
-            let average: f32 = (player.Defending.parse::<i32>().expect(&int_conversion_error) + player.StandingTackle.parse::<i32>().expect(&int_conversion_error) + player.SlidingTackle.parse::<i32>().expect(&int_conversion_error) + player.Heading.parse::<i32>().expect(&int_conversion_error) + player.Strength.parse::<i32>().expect(&int_conversion_error) + player.Aggression.parse::<i32>().expect(&int_conversion_error)) as f32 / 6.0;
+            let average: f32 = (player
+                .Defending
+                .parse::<i32>()
+                .expect(&int_conversion_error)
+                + player
+                    .StandingTackle
+                    .parse::<i32>()
+                    .expect(&int_conversion_error)
+                + player
+                    .SlidingTackle
+                    .parse::<i32>()
+                    .expect(&int_conversion_error)
+                + player.Heading.parse::<i32>().expect(&int_conversion_error)
+                + player.Strength.parse::<i32>().expect(&int_conversion_error)
+                + player
+                    .Aggression
+                    .parse::<i32>()
+                    .expect(&int_conversion_error)) as f32
+                / 6.0;
             let average = average.round() as i32;
 
             if strict {
@@ -35,18 +55,37 @@ fn main() {
                     println!("{} is wrong", player.Name);
                 }
             } else {
-                if average <= (player.OVR.parse::<i32>().expect(&int_conversion_error) + 3) && average >= (player.OVR.parse::<i32>().expect(&int_conversion_error)) - 3 {
+                if average <= (player.OVR.parse::<i32>().expect(&int_conversion_error) + 3)
+                    && average >= (player.OVR.parse::<i32>().expect(&int_conversion_error)) - 3
+                {
                     correct += 1;
                     println!("{} is correct", player.Name);
                 } else {
                     wrong += 1;
-                    diff += (average - player.OVR.parse::<i32>().expect(&int_conversion_error)).abs();
+                    diff +=
+                        (average - player.OVR.parse::<i32>().expect(&int_conversion_error)).abs();
                     println!("{}", diff);
                 }
             }
             diff = 0;
         } else if player.POS == "CM" || player.POS == "CDM" || player.POS == "CAM" {
-            let average: f32 = (player.Passing.parse::<i32>().expect(&int_conversion_error) + player.Dribbling.parse::<i32>().expect(&int_conversion_error) + player.Vision.parse::<i32>().expect(&int_conversion_error) + player.Vision.parse::<i32>().expect(&int_conversion_error) + player.Crossing.parse::<i32>().expect(&int_conversion_error) + player.ShortPassing.parse::<i32>().expect(&int_conversion_error) + player.LongPassing.parse::<i32>().expect(&int_conversion_error)) as f32 / 6.0;
+            let average: f32 = (player.Passing.parse::<i32>().expect(&int_conversion_error)
+                + player
+                    .Dribbling
+                    .parse::<i32>()
+                    .expect(&int_conversion_error)
+                + player.Vision.parse::<i32>().expect(&int_conversion_error)
+                + player.Vision.parse::<i32>().expect(&int_conversion_error)
+                + player.Crossing.parse::<i32>().expect(&int_conversion_error)
+                + player
+                    .ShortPassing
+                    .parse::<i32>()
+                    .expect(&int_conversion_error)
+                + player
+                    .LongPassing
+                    .parse::<i32>()
+                    .expect(&int_conversion_error)) as f32
+                / 6.0;
             let average = average.round() as i32;
 
             if strict {
@@ -58,12 +97,15 @@ fn main() {
                     println!("{} is wrong", player.Name);
                 }
             } else {
-                if average <= (player.OVR.parse::<i32>().expect(&int_conversion_error)) + 3 && average >= (player.OVR.parse::<i32>().expect(&int_conversion_error)) - 3 {
+                if average <= (player.OVR.parse::<i32>().expect(&int_conversion_error)) + 3
+                    && average >= (player.OVR.parse::<i32>().expect(&int_conversion_error)) - 3
+                {
                     correct += 1;
                     println!("{} is correct", player.Name);
                 } else {
                     wrong += 1;
-                    diff += (average - player.OVR.parse::<i32>().expect(&int_conversion_error)).abs();
+                    diff +=
+                        (average - player.OVR.parse::<i32>().expect(&int_conversion_error)).abs();
                     println!("{}", diff);
                 }
             }
